ba453afcec078c5e86bb295af5eb244bdb19be6b
aliyeff-f <eliyevnurlan811@gmail.com>
2024-04-30 23:21:34 +0400

    header

--- Volti/assets/js/script.js
+++ Volti/assets/js/script.js
@@ -1,22 +1,12 @@
 'use strict';
 
 
-
-/**
- * #PRELOADING
- */
-
 const loadElement = document.querySelector("[data-preloader]");
 
 window.addEventListener("load", function () {
   loadElement.classList.add("loaded");
 });
 
-
-
-/**
- * #MOBILE NAVBAR TOGGLE
- */
 
 const navbar = document.querySelector("[data-navbar]");
 const navToggler = document.querySelector("[data-nav-toggler]");
@@ -28,11 +18,6 @@ const toggleNavbar = function () {
 
 navToggler.addEventListener("click", toggleNavbar);
 
-
-
-/**
- *  #HEADER
- */
 
 const header = document.querySelector("[data-header]");
 const backTopBtn = document.querySelector("[data-go-top-btn]");
@@ -46,3 +31,18 @@ window.addEventListener("scroll", function () {
     backTopBtn.classList.remove("active");
   }
 });
+
+const revealElements = document.querySelectorAll("[data-reveal]");
+
+const scrollReveal = function () {
+  for (let i = 0, x = revealElements.length; i < x; i++) {
+    if (revealElements[i].getBoundingClientRect().top < window.innerHeight / 1.2) {
+      revealElements[i].classList.add("revealed");
+    } else {
+      revealElements[i].classList.remove("revealed");
+    }
+  }
+}
+
+window.addEventListener("scroll", scrollReveal);
+window.addEventListener("load", scrollReveal);
